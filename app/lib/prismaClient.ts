f77b8fb6b120794178f70935f9ca3f7f3a489c6a
chocobo333 <chocobo1355@gmail.com>
2024-08-18 18:06:35 +0900

    add create req to prisma client

--- app/lib/prismaClient.ts
+++ app/lib/prismaClient.ts
@@ -5,6 +5,16 @@ class GlobalPrisma {
   async findAll() {
     return await prisma.toDo.findMany();
   }
+
+  async create(body: string) {
+    await prisma.toDo.create({
+      data: {
+        body: body,
+        deadline: new Date(),
+      },
+    });
+    return;
+  }
 }
 
 export const globalPrisma = new GlobalPrisma();
